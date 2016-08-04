3ac7cf889c950d8704c2864e7e38ad2227b75c62
Jeonggyu <goodsogi@gmail.com>
2016-08-04 11:43:21 +0900

    first commit

---
+++ gen/com/example/dotnetcommunicationexample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.dotnetcommunicationexample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
